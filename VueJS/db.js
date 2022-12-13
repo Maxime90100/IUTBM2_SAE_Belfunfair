93b4039ccaeea5178477c51b14b32273e4f87f15
Maxime THEVENEAU <91722864+Maxime90100@users.noreply.github.com>
2022-12-13 19:24:13 +0100

    Update db.js

--- VueJS/db.js
+++ VueJS/db.js
@@ -1,8 +1,8 @@
 import Pool from "pg"
 export const pool = new Pool.Pool({
-    user:"",
+    user:"maxime",
     host:"localhost",
-    database:"",
-    password:"",
+    database:"bdd_sae_belfunfair",
+    password:"2903",
     port:"5432"
 });
